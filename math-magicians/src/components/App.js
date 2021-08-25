7bb9bdf08c5baccb717299ad7bc2f6d532b3cec8
JbirdL86 <jgonzalez.wj@gmail.com>
2021-08-25 11:30:22 -0600

    Render calculator app

---
+++ math-magicians/src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Calculator from './calculator.js'
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <Calculator />
+    )
+  }
+}
+
+export default App;
