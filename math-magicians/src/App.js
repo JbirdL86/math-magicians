488e65ae46f9c8f50be4bdfba12b78458f8de05e
JbirdL86 <jgonzalez.wj@gmail.com>
2021-08-25 13:56:12 -0600

    Update README file and change some files directory

--- math-magicians/src/App.js
+++ math-magicians/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import Calculator from './calculator.js'
+import Calculator from './components/calculator.js'
 
 class App extends React.Component {
   
